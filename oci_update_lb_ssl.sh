f60198a66bcce263e21c0514fe89d96e65df9b21
Dillen Hiroyuki <yuki9431@gmail.com>
2022-07-01 08:56:03 +0000

    add log output process Create Certificates process

--- oci_update_lb_ssl.sh
+++ oci_update_lb_ssl.sh
@@ -5,15 +5,33 @@ OCI_CLI=/root/bin/oci
 # Import Config
 cd $(dirname "$0")
 . oci_update_lb_ssl.conf
+. output_log.sh
 
-# certbot
-# SSL証明書を取得
+# Redirect stdout
+exec 2> ${LOGFILE}
 
-# TODO
+# Check Certificates
+if [ -e ${private-key-file} ]; then
+  log "not fount ${private-key-file}"
+  exit 1
+fi
+
+if [ -e ${public-certificate-file} ]; then
+  log "not fount ${public-certificate-file}"
+  exit 1
+fi
+
 
 # Create OCI Certificates
+certificate-name="letsencrypt-$(date +"%Y%m%d")"
 
-# TODO
+${OCI_CLI} lb certificate create \
+    --certificate-name ${certificate-name} \
+    --load-balancer-id ${load-balancer-id} \
+    --private-key-file ${private-key-file}
+    --public-certificate-file ${public-certificate-file}
+    --wait-for-state "SUCCEEDED"
+
 
 # Update OCI Load Balancer Listeners
 ${OCI_CLI} lb listener update \
@@ -23,6 +41,8 @@ ${OCI_CLI} lb listener update \
     --port ${port} \
     --protocol ${protocol} \
     --routing-policy-name ${routing-policy-name} \
-    --ssl-certificate-name ${ssl-certificate-name} \
+    --ssl-certificate-name ${certificate-name} \
     --wait-for-state "SUCCEEDED"
     --force \
+
+    # TODO #1 Delete old Certificates
