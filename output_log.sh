a2d4dd559931c8200b8661c2d4736ba76313a657
Dillen Hiroyuki <yuki9431@gmail.com>
2022-07-04 04:41:30 +0000

    fix white space

--- output_log.sh
+++ output_log.sh
@@ -2,6 +2,6 @@
 
 readonly PROCNAME=${0##*/}
 function log() {
-  local fname=${BASH_SOURCE[1]##*/}
-  echo -e "$(date '+%Y-%m-%dT%H:%M:%S') ${PROCNAME} (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $@" | tee -a ${LOGFILE}
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') ${PROCNAME} (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $@" | tee -a ${LOGFILE}
 }
